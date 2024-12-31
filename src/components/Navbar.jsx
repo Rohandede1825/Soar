e4708c533f1c4d819c719d8ac6ef1441adfd10c4
Rohandede1825 <rohandede97@gmail.com>
2024-12-31 22:48:38 +0530

    pic size and overiew button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <header className="w-full  h-[101px] bg-white flex items-center justify-between px-6 shadow-md fixed top-0 left-0 z-50">
+    <header className="w-full  h-[101px] bg-white flex items-center justify-between px-3 shadow-md fixed top-0 left-0 z-50">
       {/* Logo Section */}
       <div className="items-center space-x-3 flex-1 hidden lg:flex">
         <FaClipboardList />
@@ -25,12 +25,12 @@ const Navbar = () => {
       <div onClick={toggleSidebar} className="ThreeLine text-5xl gap-7 md:hidden">
         <VscThreeBars />
       </div>
-      <div className="md:flex flex-1 justify-start mr-24 md:mr-72">
+      <div className="md:flex flex-1  mr-24 md:mr-72">
         <h1 className="text-3xl  md:text-2xl font-bold text-gray-800  ml-3">Overview</h1>
       </div>
 
       {/* Right Section (Search Bar and User Controls) */}
-      <div className="flex items-center space-x-6 flex-1 justify-end m-5">
+      <div className="flex items-center space-x-6 flex-1 justify-end">
         <input
           type="text"
           placeholder="Search for something"
@@ -48,9 +48,9 @@ const Navbar = () => {
         <img
           src="https://media.istockphoto.com/id/1354842602/photo/portrait-of-a-young-businesswoman-working-on-a-laptop-in-an-office.jpg?s=612x612&w=0&k=20&c=kfP1g2712RiaxsDriIxFo363ARlaL2D591s-22CnIo8="
           alt="User profile"
-          className="rounded-full cursor-pointer hover:opacity-80  "
-          width="90"
-          height="90"
+          className="rounded-full cursor-pointer hover:opacity-80 ml-3 w-[60px] h-[60px] mr-7"
+          width=""
+          height=""
         />
       </div>
 
